6262c621345274d2e2a09cb66a718a9e8a17696d
Barb King <barbarak98@gmail.com>
2018-04-18 18:48:59 -0500

    updating views

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+# Lets admin know Question objects have admin interface
+
+from .models import Question
+
+admin.site.register(Question)
